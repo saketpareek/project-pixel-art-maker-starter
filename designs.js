fa9b4a3439b9f6b7299771937310126c49318430
Saket Pareek <saket.pareek90@gmail.com>
2018-04-05 16:18:21 +0530

    Added main function makeGrid() to create dynamic table

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
 // Select color input
+let selectedColor;
+
 // Select size input
+let n, m;
+
+// Select submit button
+const submitButton = $('#sizePicker').children().last();
+
+function makeGrid(evt) {
+  // Clearing table if already exist
+  if ($('tr').length) {
+    $('tr').remove();
+  }
+  // Preventing default action of the browser
+  evt.preventDefault();
+  // Setting n and m variable to height and width
+  n = $('#inputHeight').val();
+  m = $('#inputWeight').val();
+  // for loop to create rows to the number of n
+  for (let i = 1; i <= n; i++) {
+    $('#pixelCanvas').append('<tr></tr>');
+  }
+  //each loop to assign cells to each row to the number of m
+  $('tr').each(function(){
+    for (let i = 1; i <= m; i++) {
+      $(this).append('<td></td>');
+    }
+    // Selecting each cell and filling it with color
+  });
+}
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
+$(submitButton).on('click', makeGrid);
